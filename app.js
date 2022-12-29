310a35f31550a57203b1998e8702292275a5e669
Olasmart-Tech <eweleyeolamide10@gmail.com>
2022-12-28 16:22:17 -0800

    sign up form completed

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const env = require('dotenv').config();
+const bodyParser = require('body-parser');
 // const { PORT } = process.env;
 const port = process.env.PORT;
 
 
 const app = express()
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.post("/", (req, res) => {
+    let fName = req.body.firstName;
+    let lName = req.body.lastName;
+    let email = req.body.email;
+
+console.log( fName, lName, email);
+
+})
 
 app.listen(port, () => {
     console.log(`server is running on port ${port}`);
