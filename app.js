f1fa82e1961c88454a5525681322d0959eb43a6e
Olasmart-Tech <eweleyeolamide10@gmail.com>
2023-01-06 16:56:12 -0800

    added api from external server

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const env = require('dotenv').config();
 const bodyParser = require('body-parser');
+const https = require('https')
 // const { PORT } = process.env;
 const port = process.env.PORT;
 
@@ -14,12 +15,48 @@ app.get("/", (req, res) => {
 })
 
 app.post("/", (req, res) => {
-    let fName = req.body.firstName;
-    let lName = req.body.lastName;
-    let email = req.body.email;
-    let username = req.body.username;
-    console.log(fName, lName, email, username);
-    res.redirect("/success");
+    try {
+        let fName = req.body.firstName;
+        let lName = req.body.lastName;
+        let email = req.body.email;
+        let username = req.body.username;
+
+        let data = {
+            member: [
+                {
+                    email_address: email,
+                    status: "subscribed",
+                    merge_fields: {
+                        FNAME: firstName,
+                        LNAME: lastName
+                    }
+                }
+            ]
+        }
+
+        const jsonData = JSON.stringify(data);
+
+        const url = process.env.URL;
+
+        const Options = {
+            method: "POST",
+            auth: process.env.API_key - Auth
+        }
+
+        const request = https.request(url, Options, (resp) => {
+            resp.on("data", (data) => {
+                JSON.parse(data);
+            })
+        })
+        request.write(jsonData);
+        request.end();
+
+        // console.log(fName, lName, email, username);
+        res.redirect("/success");
+    } catch (error) {
+        console.log(error.message);
+    }
+
 })
 
 app.get("/success", (req, res) => {
